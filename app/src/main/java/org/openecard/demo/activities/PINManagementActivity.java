37143e9a6db0bd23f4298a7cc9cb086ec51cada7
SebastianSchuberth <sebastian.schuberth@ecsec.de>
2018-08-16 14:15:04 +0200

    Add PIN Management

---
+++ app/src/main/java/org/openecard/demo/activities/PINManagementActivity.java
@@ -0,0 +1,321 @@
+package org.openecard.demo.activities;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.Fragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import org.openecard.android.activation.ActivationResult;
+import org.openecard.android.activation.PinMgmtActivationHandler;
+import org.openecard.demo.R;
+import org.openecard.demo.fragments.CANInputFragment;
+import org.openecard.demo.fragments.FailureFragment;
+import org.openecard.demo.fragments.GenericInputFragment;
+import org.openecard.demo.fragments.InitFragment;
+import org.openecard.demo.fragments.PINChangeFragment;
+import org.openecard.demo.fragments.PUKInputFragment;
+import org.openecard.demo.fragments.WaitFragment;
+import org.openecard.gui.android.pinmanagement.PINManagementGui;
+import org.openecard.gui.android.pinmanagement.PinStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PINManagementActivity extends AppCompatActivity {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PINManagementActivity.class);
+
+
+    private final PinMgmtActivationHandler<PINManagementActivity> activationImpl;
+    private PINManagementGui pinMngGui;
+    private Button cancelBtn;
+
+    public PINManagementActivity() {
+        this.activationImpl = new ActivationImpl();
+    }
+
+    private class ActivationImpl extends PinMgmtActivationHandler<PINManagementActivity> {
+
+        public ActivationImpl() {
+            super(PINManagementActivity.this);
+        }
+
+        @Override
+        public void onGuiIfaceSet(PINManagementGui gui) {
+            PINManagementActivity.this.pinMngGui = gui;
+            initPinChangeGui();
+        }
+
+        @Override
+        public void onAuthenticationFailure(ActivationResult result) {
+
+        }
+
+        @Nullable
+        @Override
+        public Dialog showCardRemoveDialog() {
+            AlertDialog dialog = new AlertDialog.Builder(PINManagementActivity.this)
+                    .setTitle("Remove the Card")
+                    .setMessage("Please remove the identity card.")
+                    .setNeutralButton("Proceed", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create();
+            dialog.show();
+            return dialog;
+        }
+    }
+
+    public void onPINIsRequired(PinStatus status) {
+        PINChangeFragment fragment = new PINChangeFragment();
+        fragment.setStatus(status);
+
+        // show PINChangeFragment
+        getFragmentManager().beginTransaction()
+                .replace(R.id.fragment, fragment).addToBackStack(null).commitAllowingStateLoss();
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        //deactivate
+    }
+
+
+
+    ///
+    /// Callback handlers from Activity which have to be forwarded to the Activation implementation
+    ///
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        activationImpl.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        activationImpl.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        activationImpl.onPause();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom);
+
+        cancelBtn = findViewById(R.id.cancelBtn);
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                LOG.info("Cancel pressed");
+                cancelBtn.setEnabled(false);
+                cancelBtn.setClickable(false);
+
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (pinMngGui != null) {
+                            pinMngGui.cancel();
+                        }
+                    }
+                }, 100);
+            }
+        });
+
+        if (findViewById(R.id.fragment) != null) {
+            // show InitFragment
+            Fragment fragment = new InitFragment();
+            cancelBtn.setVisibility(View.VISIBLE);
+            fragment.setArguments(getIntent().getExtras());
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.fragment, fragment).addToBackStack(null).commit();
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        activationImpl.onResume();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        activationImpl.onNewIntent(intent);
+        // if you receive a nfc tag, disable the cancel button until the next fragment comes in
+        //disableCancel();
+
+        if (findViewById(R.id.fragment) != null) {
+            // show InitFragment
+            Fragment fragment = new WaitFragment();
+            cancelBtn.setVisibility(View.VISIBLE);
+            fragment.setArguments(getIntent().getExtras());
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.fragment, fragment).addToBackStack(null).commit();
+        }
+    }
+
+    public void onCANIsRequired(boolean triedBefore){
+        GenericInputFragment fragment = new CANInputFragment();
+
+        if(triedBefore){
+            fragment.setMessage("The entered CAN was wrong, please try again.");
+        }
+
+        // show CANInput
+        getFragmentManager().beginTransaction()
+                .replace(R.id.fragment, fragment).addToBackStack(null).commitAllowingStateLoss();
+    }
+
+    public void onPUKIsRequired(boolean triedBefore){
+        GenericInputFragment fragment = new PUKInputFragment();
+
+        if(triedBefore){
+            fragment.setMessage("The entered PUK was wrong, please try again.");
+        }
+
+        // show PUKInput
+        getFragmentManager().beginTransaction()
+                .replace(R.id.fragment, fragment).addToBackStack(null).commitAllowingStateLoss();
+    }
+
+    public void enterCan(String can){
+        try {
+            boolean canCorrect = pinMngGui.enterCan(can);
+            LOG.info("CAN correct: {}", canCorrect);
+
+            if(canCorrect){
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            onPINIsRequired(pinMngGui.getPinStatus());
+                        } catch (InterruptedException ex) {
+                            LOG.error(ex.getMessage(), ex);
+                        }
+                    }
+                });
+            } else {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        onCANIsRequired(true);
+                    }
+                });
+            }
+
+        } catch (InterruptedException ex) {
+            LOG.error(ex.getMessage(), ex);
+        }
+    }
+
+    public void enterPUK(String puk){
+        try {
+            boolean pukCorrect = pinMngGui.unblockPin(puk);
+            LOG.info("PUK correct: {}", pukCorrect);
+
+            if(!pukCorrect){
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        onPUKIsRequired(true);
+                    }
+                });
+            } else {
+                showMessageFragment("PIN was successful unblocked.");
+            }
+
+        } catch (InterruptedException ex) {
+            LOG.error(ex.getMessage(), ex);
+        }
+    }
+
+    public void changePin(String oldPin, String newPin){
+        try {
+            LOG.info("Perform PIN change...");
+            boolean changeSuccessful = pinMngGui.changePin(oldPin, newPin);
+            LOG.info("PINChange was successful: {}", changeSuccessful);
+
+            if(!changeSuccessful) {
+                initPinChangeGui();
+            } else {
+                showMessageFragment("Your PIN was changed successfully.");
+                pinMngGui.cancel();
+            }
+
+
+        } catch (InterruptedException ex) {
+            LOG.error(ex.getMessage(), ex);
+        }
+    }
+
+    private void showMessageFragment(String msg) {
+        FailureFragment fragment = new FailureFragment();
+        fragment.setErrorMessage(msg);
+
+        // show ServerDataFragment
+        getFragmentManager().beginTransaction()
+                .replace(R.id.fragment, fragment).addToBackStack(null).commitAllowingStateLoss();
+    }
+
+    private void initPinChangeGui(){
+
+        try {
+            final PinStatus pinStatus = pinMngGui.getPinStatus();
+            LOG.info("PIN status: {}", pinStatus);
+
+            if(pinStatus.isNormalPinEntry()){
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        onPINIsRequired(pinStatus);
+                    }
+                });
+            }
+            else if(pinStatus.needsCan()){
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        onCANIsRequired(false);
+                    }
+                });
+            }
+
+            else if (pinStatus.needsPuk()){
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        onPUKIsRequired(false);
+                    }
+                });
+            } else if (pinStatus.isDead()){
+                String msg = String.format("PIN Status is '%s'.", pinStatus);
+                showMessageFragment(msg);
+                LOG.error(msg);
+                pinMngGui.cancel();
+            }
+
+        } catch (InterruptedException ex) {
+            LOG.error(ex.getMessage(), ex);
+        }
+    }
+}
---
+++ app/src/main/java/org/openecard/demo/fragments/PINChangeFragment.java
@@ -0,0 +1,128 @@
+package org.openecard.demo.fragments;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.openecard.demo.R;
+import org.openecard.demo.activities.PINManagementActivity;
+import org.openecard.gui.android.pinmanagement.PinStatus;
+
+
+public class PINChangeFragment extends Fragment {
+
+    private static final String PERFORM_PIN_CHANGE = "Please wait while PIN is changed...";
+    private static final String WRONG_PIN = "The entered PIN was wrong, please try again.";
+    private static final String MISMATCHING_PINS = "The two new PINs do not match.";
+
+    private PinStatus status;
+    private EditText pinText;
+    private EditText newPin;
+    private EditText newPinConfirm;
+
+    public void setStatus(PinStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_pin_change, container, false);
+
+        final TextView logLabel = view.findViewById(R.id.txtLog);
+        logLabel.setVisibility(View.INVISIBLE);
+
+        pinText = view.findViewById(R.id.pinInput);
+        pinText.setEnabled(true);
+        pinText.setFocusable(true);
+        //pinText.requestFocus();
+
+        newPin = view.findViewById(R.id.newPinInput);
+        newPinConfirm = view.findViewById(R.id.newPinInputConfirm);
+
+        final Button buttonContinue = view.findViewById(R.id.btnPINInput);
+        buttonContinue.setEnabled(false);
+
+
+        if (status != null) {
+            if (status == PinStatus.RC2) {
+                logLabel.setVisibility(View.VISIBLE);
+                logLabel.setText(WRONG_PIN);
+            }
+        }
+
+        TextWatcher textChangeListener = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                buttonContinue.setEnabled(canContinue());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        };
+
+        pinText.addTextChangedListener(textChangeListener);
+        newPin.addTextChangedListener(textChangeListener);
+        newPinConfirm.addTextChangedListener(textChangeListener);
+
+        buttonContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Activity activity = getActivity();
+                if (activity instanceof PINManagementActivity) {
+
+                    final String newPIN = newPin.getText().toString();
+                    final String newPIN2 = newPinConfirm.getText().toString();
+
+                    if (!newPIN.equals(newPIN2)) {
+                        logLabel.setText(MISMATCHING_PINS);
+                        logLabel.setVisibility(View.VISIBLE);
+                        return;
+                    }
+
+                    final String pin = pinText.getText().toString();
+
+                    if (pin.length() == 6) {
+                        buttonContinue.setEnabled(false);
+                        pinText.setEnabled(false);
+                        pinText.setFocusable(false);
+                        newPin.setEnabled(false);
+                        newPin.setFocusable(false);
+                        newPinConfirm.setEnabled(false);
+                        newPinConfirm.setFocusable(false);
+                        logLabel.setText(PERFORM_PIN_CHANGE);
+                        logLabel.setVisibility(View.VISIBLE);
+
+                        new Thread(new Runnable() {
+                            public void run() {
+                                ((PINManagementActivity) activity).changePin(pin, newPIN);
+                            }
+                        }).start();
+                    }
+                }
+            }
+        });
+
+        return view;
+    }
+
+    private boolean canContinue(){
+        boolean pinLengthCorrect = pinText.getText().toString().length() == 6
+                || pinText.getText().toString().length() == 5; //transport PIN
+        boolean newPinLengthCorrect = (newPin.getText().toString().length() == 6
+                && newPinConfirm.getText().toString().length() == 6);
+        return pinLengthCorrect && newPinLengthCorrect;
+    }
+}
