5338a78370f9da7ffceb81eeb6c33f21a7df4b26
Tobias Wich <tobias.wich@ecsec.de>
2019-09-04 12:33:31 +0200

    Update to androidx

--- app/src/main/java/org/openecard/demo/activities/PINManagementActivity.java
+++ app/src/main/java/org/openecard/demo/activities/PINManagementActivity.java
@@ -27,8 +27,8 @@ import android.app.Dialog;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
